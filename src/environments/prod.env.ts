49f1f32d69e64c3937bd8dbf9cb19b4c42a38fd4
jaskirat-singh-0403 <100020768+jaskirat-singh-0403@users.noreply.github.com>
2022-12-19 11:26:05 +0530

    Update prod.env.ts

--- src/environments/prod.env.ts
+++ src/environments/prod.env.ts
@@ -1,5 +1,5 @@
 import { Environment } from "./env";
 
 export const ProdEnvironment:Environment={
-    db_url:"mongodb+srv://test:test123@cluster-my.ejvqm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    db_url:"userurl"
 }
